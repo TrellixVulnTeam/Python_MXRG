f2d5c6bcd7eb13e7a7bc7407e0b89e296c494e2e
hyeona kim <>
2018-05-24 11:49:02 +0900

    ex_06_01

---
+++ PY4E/ex_06_01.py
@@ -0,0 +1,12 @@
+str = 'X-DSPM-Confidence: 0.8475'
+
+ipos = str.find(':')
+# print(ipos)
+
+piced = str[ipos+2:]
+# print(piced)
+value = float(piced)
+print(value)
+# print(value+42)
+# print(piced+42)
+
